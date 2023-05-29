a6359384ddde8880bbeecba7c483f4cd513c3030
NikolaNB1 <babicn19@gmail.com>
2023-05-29 19:58:58 +0200

    drugi zadatak

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
+import AppPosts from "./components/AppPosts";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/posts" />}></Route>
+      <Route path="/posts" element={<AppPosts />}></Route>
     </Routes>
   );
 }
---
+++ src/components/AppPosts.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { getPosts } from "../service/blogsService";
+
+const AppPosts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts().then(({ data }) => setPosts(data));
+  }, []);
+
+  return (
+    <div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <table
+          className="table table-striped table-hover"
+          style={{ width: "300px", textAlign: "center" }}
+        >
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Text</th>
+              <th>Created at</th>
+            </tr>
+          </thead>
+          <tbody>
+            {posts.map((post, id) => (
+              <tr key={id}>
+                <td>{post.title}</td>
+                <td>{post.text}</td>
+                <td>{post.createdAt}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+export default AppPosts;
